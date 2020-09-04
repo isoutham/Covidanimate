b7fbedecb06bcd63ec6295a7cee3195ae8c8f028
Ian Southam <isoutham@schubergphilis.com>
2020-09-04 17:28:37 +0200

    Add some data sources for the UK

--- libs/TimeSeriesNL.py
+++ libs/TimeSeriesNL.py
@@ -2,15 +2,12 @@ import pandas as pd
 import geopandas as gpd
 import csv
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-class TimeSeriesNL(object):
+class TimeSeries(object):
 
-    MUNICIPAL = "../CoronaWatchNL/data-geo/data-municipal/RIVM_NL_municipal.csv"
-    MAPS = {'Gemeentecode': 'maps/gemeente-2019.geojson',
-            'Provincienaam': 'maps/provincies.geojson'
-            }
-    key = 'Gemeentecode'
     RECOVERY_WINDOW = 14
 
     def __init__(self):
@@ -18,6 +15,92 @@ class TimeSeriesNL(object):
         self.gemsdf = None
         self.target = 'Totaal'
         self.get_maps()
+
+    def get_maps(self):
+        if self.map_df is None:
+            self.map_df = gpd.read_file(self.MAPS[self.key])
+        return self.map_df
+
+    def get_max(self):
+        maxc = ''
+        for column in self.merged.columns:
+            if column.startswith('20'):
+                if column > maxc:
+                    maxc = column
+        maxval = self.merged[maxc].max()
+        return int(math.ceil(maxval / 100.0)) * 100
+
+    def set_map(self, df):
+        self.map_df = df
+
+    def get_merged(self):
+        return self.merged
+
+    def get_cc(self):
+        return self.cc
+
+class TimeSeriesUK(TimeSeries):
+
+    MUNICIPAL = "data/uk.csv"
+    MAPS = {'Counties': 'maps/uk_counties.geojson'
+            }
+    key = 'Counties'
+    cc = 'England'
+
+    def __init__(self):
+        super(TimeSeriesUK, self).__init__()
+
+    def process(self):
+        reader = csv.DictReader(open(self.MUNICIPAL))
+        data = {}
+        dte = ''
+        h = None
+        for row in reader:
+            # date', 'areaName', 'areaCode', 'cumCasesBySpecimenDate'
+            try:
+                cnt = float(row['cumCasesBySpecimenDate'])
+            except:
+                cnt = 0
+            if not row['areaCode'].startswith('E'):
+                continue
+            dte = row['date']
+            if row['date'] not in data:
+                data[row['date']] = {}
+            if row['areaCode'] not in data[row['date']]:
+                data[row['date']][row['areaCode']] = 0
+
+                if cnt > 0:
+                    data[dte][row['areaCode']] += cnt
+        df = pd.DataFrame.from_dict(data, orient='columns')
+        self.df2 = df
+
+    def merge(self):
+        self.merged = self.map_df.join(self.df2)
+        self.merged.replace(r'\s+', np.nan, regex=True, inplace=True)
+        self.merged.fillna(0, inplace=True)
+        self.do_calculations()
+
+    def do_calculations(self):
+        self.merged['population'] = self.merged['population'].str.replace('.','')
+        self.merged['population'] = self.merged['population'].astype(float)
+        for column in self.merged.columns:
+            if column.startswith('20'):
+                self.merged[column] = self.merged[column] / \
+                    (self.merged['population'] / 100000)
+
+
+class TimeSeriesNL(TimeSeries):
+
+    MUNICIPAL = "../CoronaWatchNL/data-geo/data-municipal/RIVM_NL_municipal.csv"
+    MAPS = {'Gemeentecode': 'maps/gemeente-2019.geojson',
+            'Provincienaam': 'maps/provincies.geojson'
+            }
+    key = 'Gemeentecode'
+
+    cc = 'The Netherlands'
+
+    def __init__(self):
+        super(TimeSeriesNL, self).__init__()
 
     def merge(self):
         self.merged = self.map_df.join(self.df2)
@@ -28,21 +111,6 @@ class TimeSeriesNL(object):
             if column.startswith('20'):
                 self.merged[column] = self.merged[column] / \
                     (self.merged['aantal'] / 100000)
-
-    def get_max(self):
-        maxval = self.merged[self.merged.columns[-1]].max()
-        return int(math.ceil(maxval / 100.0)) * 100
-
-    def get_maps(self):
-        if self.map_df is None:
-            self.map_df = gpd.read_file(self.MAPS[self.key])
-        return self.map_df
-
-    def set_map(self, df):
-        self.map_df = df
-
-    def get_merged(self):
-        return self.merged
 
     def process(self):
         reader = csv.DictReader(open(self.MUNICIPAL))

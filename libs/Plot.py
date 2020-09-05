a7ec6c270b811cf9f6f85f729943eeff8af3b4c1
Ian Southam <isoutham@schubergphilis.com>
2020-09-05 14:55:07 +0200

    Fixed up all those pylint gripes

--- libs/Plot.py
+++ libs/Plot.py
@@ -1,7 +1,8 @@
+"""Plotting routines"""
 import os
-import matplotlib.pyplot as plt
 import glob
 import imageio
+import matplotlib.pyplot as plt
 
 
 class Plot:
@@ -12,19 +13,18 @@ class Plot:
     FIGURES = 'figures3/'
 
     def __init__(self, ts):
-        self.ts = ts
+        self.timeseries = ts
         plt.style.use('fivethirtyeight')
         self.cmap = 'Oranges'
-        self.fig = None
-        self.ax = None
         files = glob.glob(self.FIGURES + '*.png')
-        for f in files:
-            os.remove(f)
-        self.vmin, self.vmax = 0, ts.get_max()
+        for file in files:
+            os.remove(file)
+        self.vmin, self.vmax = 0, self.timeseries.get_max()
 
     def make_frames(self):
+        """Create all the frames for each date in the timeseries"""
         count = 9999
-        for date in sorted(self.ts.get_merged().columns):
+        for date in sorted(self.timeseries.get_merged().columns):
             if date.startswith('2020-'):
                 self.do_plot(date)
                 count -= 1
@@ -32,18 +32,22 @@ class Plot:
                 break
 
     def do_plot(self, date):
+        """Create a single frame for a given date"""
         print(date)
-        self.fig, self.ax = plt.subplots(1, figsize=(6, 6))
-        self.ts.get_merged().plot(column=date, cmap=self.cmap, linewidth=0.8,
-                                  ax=self.ax, edgecolor='0.8', legend=True, vmax=self.vmax)
-        self.ax.axis('off')
-        plt.title('%s %s' % (self.ts.get_cc(),  date))
-        plt.figtext(0.5, 0.05, "Cases per 100.000 inhabitants", ha="center",
+        fig, axis = plt.subplots(1, figsize=(6, 6))
+        self.timeseries.get_merged().plot(column=date, cmap=self.cmap, linewidth=0.8,
+                                          ax=axis, edgecolor='0.8', legend=True, vmax=self.vmax)
+        axis.axis('off')
+        plt.title('%s %s' % (self.timeseries.get_cc(),  date))
+        plt.figtext(0.5, 0.05, "Cumulative confirmed Cases per 100.000 inhabitants", ha="center",
                     fontsize=10, bbox={"facecolor": "white", "alpha": 0.5, "pad": 5})
         plt.savefig('figures3/%s.png' % date)
-        plt.close(self.fig)
+        plt.close(fig)
 
     def animate(self):
+        """Create the animation
+           This can be also done using Malibplot but this worked better for me
+           """
         images = []
         directory = 'figures3/'
         for filename in os.listdir(directory):
@@ -52,5 +56,5 @@ class Plot:
         idata = []
         for i in sorted(images):
             idata.append(imageio.imread(i))
-        fn = '%s_choropleth.mp4' % self.ts.get_cc()
-        imageio.mimsave(fn, idata)
+        filename = '%s_choropleth.mp4' % self.timeseries.get_cc()
+        imageio.mimsave(filename, idata)
